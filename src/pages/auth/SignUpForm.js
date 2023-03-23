36b915d4fc95c6a4c5eab4176a249aea198e6b8b
Chrisfaherty <chrismactom@gmail.com>
2023-03-23 18:02:24 +0000

    Add alerts for the username input field in the sign up form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,32 +1,75 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: '',
+        password1: '',
+        password2: ''
+    })
+    const {username, password1, password2} = signUpData;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handelSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/dj-rest-auth/registeration/', signUpData)
+            history.push('/signin')
+        } catch(err){
+            setErrors(err.response?.data)
+        }
+    }
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form>
+<Form onSubmit={handleSubmit}>
   <Form.Group controlId="formBasicEmail">
     <Form.Label className="d-none">username</Form.Label>
-    <Form.Control type="text" placeholder="Username" name="username" className={styles.Input}/>
-
+    <Form.Control type="text" 
+        placeholder="Username" 
+        name="username" 
+        className={styles.Input} 
+        value={username}
+        onChange={handleChange}/>
   </Form.Group>
-
+  {error.username?.map((message, idx)=>
+    <Alert variant="warning" key={idx}>{message}</Alert>
+  )}
   <Form.Group controlId="password1">
     <Form.Label className="d-none">Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" name="password1" className={styles.Input}/>
+    <Form.Control type="password" 
+        placeholder="Password" 
+        name="password1" 
+        className={styles.Input} 
+        value={password1}
+        onChange={handleChange}/>
   </Form.Group>
   <Form.Group controlId="password2">
     <Form.Label className="d-none">Confirm password</Form.Label>
-    <Form.Control type="password" placeholder="Confirm password" name="password2" className={styles.Input}/>
+    <Form.Control type="password" 
+        placeholder="Confirm password" 
+        name="password2" 
+        className={styles.Input} 
+        value={password2}
+        onChange={handleChange}/>
   </Form.Group>
  
   <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
