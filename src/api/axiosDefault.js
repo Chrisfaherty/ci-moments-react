1dbc3365cc3ee347a5f56d3d53c185c321ba01ee
Chrisfaherty <chrismactom@gmail.com>
2023-03-24 15:20:41 +0000

    Add axios interceptors to the project

--- src/api/axiosDefault.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "https://ci-moments-react.herokuapp.com/"
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
