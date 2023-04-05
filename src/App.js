2668fa4c71177193361a624910b0dcf7027aaba2
Chrisfaherty <chrismactom@gmail.com>
2023-04-05 08:41:26 +0000

    add username form, userpasswordform and useredit form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -52,6 +55,9 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+          <Route exact path="/profiles/:id/edit/username" render={() => <UsernameForm />}/>
+          <Route exact path="/profiles/:id/edit/password" render={() => <UserPasswordForm />}/>
+          <Route exact path="/profiles/:id/edit" render={() => <ProfileEditForm />}/>
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
